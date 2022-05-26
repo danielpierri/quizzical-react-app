85bd89267c5521181ff742ce857661481927867e
Daniel Pierri <cyberpierri@gmail.com>
2022-05-26 20:16:08 -0300

    Opening social media in another tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ export default function Footer() {
   return (
     <div className="footer__container">
       <div className="footer__icons">
-        <a href="https://www.instagram.com/cyberpierri" classname="footer__ig"><img src={theme === "light" ? igLight : igDark} alt="Instagram logo" /></a>
-        <a href="https://www.linkedin.com/in/danielpierri/" classname="footer__li"><img src={theme === "light" ? liLight : liDark} alt="LinkedIn logo" /></a>
-        <a href="https://github.com/danielpierri" classname="footer__gh"><img src={theme === "light" ? ghLight : ghDark} alt="GitHub logo" /></a>
+        <a href="https://www.instagram.com/cyberpierri" target="_blank"><img src={theme === "light" ? igLight : igDark} alt="Instagram logo" /></a>
+        <a href="https://www.linkedin.com/in/danielpierri/" classname="footer__li" target="_blank"><img src={theme === "light" ? liLight : liDark} alt="LinkedIn logo" /></a>
+        <a href="https://github.com/danielpierri" classname="footer__gh" target="_blank"><img src={theme === "light" ? ghLight : ghDark} alt="GitHub logo" /></a>
       </div>
       <div className={`footer__message footer__message--${theme}`}>
         <p className="footer__phrase">QUIZ APP DEVELOPED BY <span className={`footer__name footer__name--${theme}`}>DANIEL PIERRI</span></p>
